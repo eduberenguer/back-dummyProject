cc7fe04f8158cd6e7fea50feba0c922659369bdc
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2022-02-22 10:49:16 +0100

    (feat): Add commit test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || "8000";
 
 app.get("/", (req, res) => {
-  res.status(200).send("WHATABYTE: Food For Devs");
+  res.status(200).send("WHATABYTE: TEST");
 });
 
 app.listen(port, () => {
